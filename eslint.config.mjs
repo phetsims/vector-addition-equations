65a8c6afd84bdf9f97d11f344dff7e7663ed78d5
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:15 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for vector-addition-equations.
+ * ESLint configuration for vector-addition-equations.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
