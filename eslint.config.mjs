874d0bdbaa6f5a084d5732178a41c7f16d7224c3
pixelzoom <cmalley@pixelzoom.com>
2024-11-08 16:18:31 -0700

    Revert "optimize and sort imports, https://github.com/phetsims/chipper/issues/1462"
    This reverts commit ab968ffbea80c0f79e3ede19d0594c24ee14c52f.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,8 +7,8 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import simEslintConfig from '../perennial-alias/js/eslint/config/sim.eslint.config.mjs';
 import banTSCommentConfig from '../perennial-alias/js/eslint/config/util/banTSCommentConfig.mjs';
+import simEslintConfig from '../perennial-alias/js/eslint/config/sim.eslint.config.mjs';
 
 export default [
   ...simEslintConfig,
